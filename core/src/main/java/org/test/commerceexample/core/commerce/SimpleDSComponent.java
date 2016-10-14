fc12ea303be4183b57b7e50fc07fa0b7ef19538f
Anthony Ross <anthony.ross@validic.com>
2016-10-14 07:22:26 -0400

    Initial Commit
    Everything is running based on this article:
    https://helpx.adobe.com/experience-manager/using/ecommerce62.html

---
+++ core/src/main/java/org/test/commerceexample/core/commerce/SimpleDSComponent.java
@@ -0,0 +1,29 @@
+package org.test.commerceexample.core.commerce;
+ 
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Service;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+/**
+ * A basic DS Component
+ */
+@Component(metatype=true)
+@Service
+public class SimpleDSComponent implements Runnable {
+      
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private BundleContext bundleContext;
+      
+    public void run() {
+        logger.info("Running...");
+    }
+      
+    protected void activate(ComponentContext ctx) {
+        this.bundleContext = ctx.getBundleContext();
+    }
+    protected void deactivate(ComponentContext ctx) {
+        this.bundleContext = null;
+    }
+}
